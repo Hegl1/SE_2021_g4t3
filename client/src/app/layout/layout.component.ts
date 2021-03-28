1a09df09ec2a5a9ec9d87dec1a0a18ddcb51d9ac
Matthias Thalmann <matthias.thalmann@student.uibk.ac.at>
2021-03-28 21:13:12 +0200

    feat(client): added layout and empty pages (#12)

---
+++ client/src/app/layout/layout.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tg-layout',
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.scss'],
+})
+export class LayoutComponent {
+  constructor() {}
+
+  openSettings() {
+    throw new Error('Not implemented');
+  }
+
+  openProfile() {
+    throw new Error('Not implemented');
+  }
+}
--- client/src/app/material/material.module.ts
+++ client/src/app/material/material.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [],
   imports: [],
-  exports: [],
+  exports: [MatButtonModule, MatListModule, MatIconModule],
 })
 export class MaterialModule {}
