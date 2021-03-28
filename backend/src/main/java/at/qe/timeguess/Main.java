38619cfc9eeec534146345159e2bcbaaeee961e0
Aaron Targa <aaron.targa@student.uibk.ac.at>
2021-03-28 17:01:19 +0200

    feat(backend): cleanup

--- backend/src/main/java/at/qe/timeguess/Main.java
+++ backend/src/main/java/at/qe/timeguess/Main.java
@@ -1,15 +1,9 @@
 package at.qe.timeguess;
 
-import javax.faces.webapp.FacesServlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScan.Filter;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 /**
@@ -30,22 +24,10 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
  * spring. It is reserved for the CDI-injection-mechanisms (Weld). Only add
  * CDI-managed components.
  */
-@ComponentScan(basePackages = "at.qe.timeguess", excludeFilters = @Filter(type = FilterType.REGEX, pattern = "at.qe.skeleton.ui.websockets.*"))
 public class Main extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
             SpringApplication.run(Main.class, args);
-    }
-
-    @Bean
-    public ServletRegistrationBean<FacesServlet> servletRegistrationBean() {
-            FacesServlet servlet = new FacesServlet();
-            ServletRegistrationBean<FacesServlet> servletRegistrationBean = new ServletRegistrationBean<>(servlet,
-                            "*.xhtml");
-            servletRegistrationBean.setName("Faces Servlet");
-            servletRegistrationBean.setAsyncSupported(true);
-            servletRegistrationBean.setLoadOnStartup(1);
-            return servletRegistrationBean;
     }
 
 }
