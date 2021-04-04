68226f70da490cc5d435aaa8c921952e58c3cb08
Marcel Alexander Huber <Marcel.Huber@student.uibk.ac.at>
2021-04-04 09:56:36 +0200

    refactor(backend): Split: RaspiController into Service + Controller(#32)

--- backend/src/main/java/at/qe/timeguess/controllers/RaspberryController.java
+++ backend/src/main/java/at/qe/timeguess/controllers/RaspberryController.java
@@ -1,8 +1,5 @@
 package at.qe.timeguess.controllers;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,10 +10,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import at.qe.timeguess.model.RaspberryID;
-import at.qe.timeguess.repositories.RaspberryIDRepository;
 import at.qe.timeguess.services.RandomCodeService;
-import gamelogic.Game;
+import at.qe.timeguess.services.RaspberryService;
 
 /**
  * Class that handles communication with raspberries. Also contains a mapping
@@ -32,16 +27,7 @@ public class RaspberryController {
 	private RandomCodeService codeGenerator;
 
 	@Autowired
-	private RaspberryIDRepository raspbiRepo;
-
-	/**
-	 * Map that associates raspberries with games.
-	 */
-	private Map<String, Game> gameMappings;
-
-	public RaspberryController() {
-		this.gameMappings = new HashMap<String, Game>();
-	}
+	private RaspberryService raspiService;
 
 	/**
 	 * Method that persists a new raspberry with a random id.
@@ -50,13 +36,7 @@ public class RaspberryController {
 	 */
 	@GetMapping("/register")
 	public ResponseEntity<String> registerRaspberry() {
-		String identifier;
-		do {
-			identifier = codeGenerator.generateRandomRaspberryCode(identifyerLength);
-		} while (raspbiRepo.findFirstById(identifier) != null);
-
-		raspbiRepo.save(new RaspberryID(identifier));
-
+		String identifier = raspiService.registerRaspberry();
 		return new ResponseEntity<>(identifier, HttpStatus.OK);
 	}
 
@@ -74,70 +54,13 @@ public class RaspberryController {
 		if (update < 0 || update > 11) {
 			return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
 		} else {
-			if (raspbiRepo.findFirstById(id) == null) {
-				return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
-			} else {
-				if (gameMappings.get(id) != null) {
-					gameMappings.get(id).diceUpdate(update);
-				}
+			try {
+				raspiService.updateDice(id, update);
 				return new ResponseEntity<>(null, HttpStatus.OK);
+			} catch (at.qe.timeguess.services.RaspberryService.RaspberryNotFoundException e) {
+				return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
 			}
 		}
 	}
 
-	/**
-	 * Method that registers a game for a given raspberry id.
-	 * 
-	 * @param raspbiId id of the raspberry the game should be associated with.
-	 * @param game     the game that gets registered.
-	 * @throws RaspberryAlreadyInUseException when raspberry is already assigned to
-	 *                                        a running game.
-	 */
-	public void registerGame(final String raspbiId, final Game game)
-			throws RaspberryAlreadyInUseException, RaspberryNotFoundException {
-
-		if (raspbiRepo.findFirstById(raspbiId) == null) {
-			throw new RaspberryNotFoundException();
-		}
-
-		if (gameMappings.containsKey(raspbiId)) {
-			throw new RaspberryAlreadyInUseException("Id " + raspbiId + "already in use");
-		} else {
-			gameMappings.put(raspbiId, game);
-		}
-	}
-
-	/**
-	 * Method to unregister a game of raspberry.
-	 * 
-	 * @param raspbiId Id of the raspberry that is associated with the game to be
-	 *                 unregistered.
-	 */
-	public void unregisterGame(final String raspbiId) {
-		gameMappings.remove(raspbiId);
-	}
-
-	public Map<String, Game> getGameMappings() {
-		return gameMappings;
-	}
-
-	/**
-	 * Exception for when trying to register an already registered game to a
-	 * raspberry.
-	 *
-	 */
-	public class RaspberryAlreadyInUseException extends Exception {
-
-		private static final long serialVersionUID = 1L;
-
-		public RaspberryAlreadyInUseException(final String message) {
-			super(message);
-		}
-	}
-
-	public class RaspberryNotFoundException extends Exception {
-
-		private static final long serialVersionUID = 1L;
-
-	}
 }
