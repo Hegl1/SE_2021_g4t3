aff950f839b225dfee4f5b83c299822eaa59edd2
Aaron Targa <aaron.targa@student.uibk.ac.at>
2021-04-10 18:48:43 +0200

    feat(users): adding update date to user entity

--- backend/src/main/java/at/qe/timeguess/model/User.java
+++ backend/src/main/java/at/qe/timeguess/model/User.java
@@ -1,50 +1,70 @@
 package at.qe.timeguess.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import java.util.Date;
 
 @Entity
 public class User {
 
-	@Id
-	@GeneratedValue
-	private Long id;
-	private String username;
-	@JsonIgnore
-	private String password;
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String username;
+    @JsonIgnore
+    private String password;
 
-	@Enumerated(EnumType.STRING)
-	@Column(nullable = false)
-	private UserRole role;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updateDate;
 
-	public User() {
-	}
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRole role;
 
-	public User(final String username, final String password, final UserRole role) {
-		this.username = username;
-		this.password = password;
-		this.role = role;
-	}
+    public User() {
+    }
 
-	public String getUsername() {
-		return username;
-	}
+    public User(final String username, final String password, final UserRole role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
 
-	public UserRole getRole() {
-		return role;
-	}
+    public String getUsername() {
+        return username;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public UserRole getRole() {
+        return role;
+    }
 
-	public Long getId() {
-		return id;
-	}
+    public String getPassword() {
+        return password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Date getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUpdateDate(Date updateDate) {
+        this.updateDate = updateDate;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
+    }
 }
