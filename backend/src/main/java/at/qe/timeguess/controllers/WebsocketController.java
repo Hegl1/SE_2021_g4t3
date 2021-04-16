5271b69662c8f466cc6a8438355f70d4e9055bf0
Marcel Alexander Huber <Marcel.Huber@student.uibk.ac.at>
2021-04-16 14:28:00 +0200

    refactor(backend): cleaned Websocket related items for merge

--- backend/src/main/java/at/qe/timeguess/controllers/WebsocketController.java
+++ backend/src/main/java/at/qe/timeguess/controllers/WebsocketController.java
@@ -2,30 +2,35 @@ package at.qe.timeguess.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.socket.messaging.SessionConnectedEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
 import at.qe.timeguess.dto.TemporaryTestDTO;
 
 @Controller
-public class IngameMessageController {
+public class WebsocketController {
 
 	@Autowired
 	private SimpMessagingTemplate simpMessageingTemplate;
 
-	@MessageMapping("/test")
-	public void reply(@Payload final TemporaryTestDTO message) {
-		System.out.println(message.getText());
-		simpMessageingTemplate.convertAndSend("/messagequeue/ingame", message);
+	@MessageMapping("/placeholder/{id}")
+	public void reply(@DestinationVariable final Integer id, @Payload final TemporaryTestDTO message) {
+		simpMessageingTemplate.convertAndSend("/messagequeue/ingame/" + id, message);
 	}
 
 	@EventListener
 	public void onDisconnectEvent(final SessionDisconnectEvent event) {
-		// TODO detect disconnecting user
-		System.out.println("One client disconnected");
+		// TODO detect user, do usefull stuff
+	}
+
+	@EventListener
+	public void onConnectedEvent(final SessionConnectedEvent event) {
+		// TODO detect user, do usefull stuff
 	}
 
 }
