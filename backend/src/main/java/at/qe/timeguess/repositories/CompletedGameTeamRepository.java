3dee875e7dc4f02a867c1d39f43c92975571b7c5
Thomas Klotz <t.klotz@student.uibk.ac.at>
2021-05-01 13:47:37 +0200

    feat(Repository): removed and added 2 functions

--- backend/src/main/java/at/qe/timeguess/repositories/CompletedGameTeamRepository.java
+++ backend/src/main/java/at/qe/timeguess/repositories/CompletedGameTeamRepository.java
@@ -13,11 +13,4 @@ public interface CompletedGameTeamRepository extends AbstractRepository<Complete
     @Query("Select t FROM CompletedGameTeam t WHERE :user MEMBER OF t.players")
     public List<CompletedGameTeam> findByUser(@Param("user") User user);
 
-    @Query("SELECT CompletedGame.id, end_time, start_time, category_id " +
-        "FROM CompletedGameTeam " +
-        "JOIN completed_game_attended_teams ON CompletedGameTeam.id = completed_game_attended_teams.attended_teams_id " +
-        "JOIN CompletedGame                 ON completed_game_attended_teams.completed_game_id = CompletedGame.id " +
-        "WHERE :user MEMBER OF CompletedGameTeam.players")
-    public List<CompletedGame> findCompletedGames(@Param("user") User user);
-
 }
